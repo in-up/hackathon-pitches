1110cff34ee07e42fefb1343869ccf23217a2272
in-up <inup@kyonggi.ac.kr>
2024-10-26 01:59:07 +0900

    docs: edit pubspec.yaml

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,1 +1,74 @@
-// 홈화면
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var mybox = Hive.box('localdata');
+  List<Map<String, dynamic>> mydata = [];
+
+  var myText = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getItem();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    getItem();
+  }
+
+  void addItem(Map<String, dynamic> data) async {
+    await mybox.add(data);
+    print(mybox.values);
+    getItem();
+  }
+
+  void deleteItem(int index) {
+    mybox.delete(mydata[index]['key']);
+    getItem();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('삭제되었습니다'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+
+  void getItem() {
+    setState(() {
+      mydata = mybox.keys.map((e) {
+        var res = mybox.get(e);
+        return {'key': e, 'title': res['title']};
+      }).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).canvasColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Text('테스트 텍스트')
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/data/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../app/home.dart';
+
+final Widget homeRoute = HomeScreen();
+
+final Map<String, WidgetBuilder> routes = {
+  '/home': (context) => HomeScreen(),
+
+};
